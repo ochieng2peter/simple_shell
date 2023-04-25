cef852d3513d60a3ef6d379d4567cba15ee46e59
ochieng2peter <ochieng2.peter@gmail.com>
2023-04-26 02:16:09 +0300

    0.5-task.c

---
+++ 0.5-task.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_env - Prints the current environment variables.
+ * Return: None.
+ */
+void print_env(void)
+{
+	int i;
+	char newline = '\n';
+	char *env_var;
+
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		env_var = environ[i];
+		while (*env_var != '\0')
+		{
+			if (write(STDOUT_FILENO, env_var, 1) < 0)
+			{
+				perror("write failed");
+				exit(EXIT_FAILURE);
+			}
+			env_var++;
+		}
+		if (write(STDOUT_FILENO, &newline, 1) < 0)
+		{
+			perror("write failed");
+			exit(EXIT_FAILURE);
+		}
+	}
+}
