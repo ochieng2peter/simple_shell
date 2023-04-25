c55538afb8ac9fdddbc2d9fa688a61e9ec73c07f
ochieng2peter <ochieng2.peter@gmail.com>
2023-04-25 23:34:13 +0300

    main.h more edits

--- main.h
+++ main.h
@@ -10,7 +10,15 @@
 
 
 #define MAX_COMMAND_LENGTH 100
-#define MAX_ARGS_LENGTH 50
-#define MAX_PATH_LENGTH 100
+#define MAX_ARGS_LENGTH 50 
+#define MAX_PATH_LENGTH 1000
 
-#endif
+/*functions*/
+
+void execute_command(char *command, int (*str_cmp)(const char *, const char *));
+void add_path_dir(const char *dir);
+int my_strcmp(const char *s1, const char *s2);
+
+/*char *_getenv(const char *name, char **env);*/
+
+#endif /* MAIN_H */
