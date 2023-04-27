b9b8d90d46373a458c992b2f667a2c858629f641
ochieng2peter <ochieng2.peter@gmail.com>
2023-04-27 15:22:02 +0300

    edits to main.h

--- main.h
+++ main.h
@@ -10,15 +10,15 @@
 
 
 #define MAX_COMMAND_LENGTH 100
-#define MAX_ARGS_LENGTH 50 
+#define MAX_ARGS_LENGTH 50
 #define MAX_PATH_LENGTH 1000
 
 
 extern char **environ;
 
 /*functions*/
-
-void execute_command(char *command, int (*str_cmp)(const char *, const char *));
+typedef int (*str_cmp_func)(const char *, const char *);
+void execute_command(char *command, str_cmp_func str_cmp);
 void add_path_dir(const char *dir);
 int my_strcmp(const char *s1, const char *s2);
 void print_env(void);
